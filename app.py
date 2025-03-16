688ea1fc8f93652b4347b7bc3add11e5c6ba6ce1
shakeelmahdhy <shakeelahmedmahdhy@gmail.com>
2025-03-16 15:47:34 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://elysia-ai.onrender.com/predict"
 st.set_page_config(page_title="Elysia AI - Emotion Chat", page_icon="💬", layout="wide")
 
 st.markdown("""
